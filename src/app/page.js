37c70a98956b7b39232b165ec36da70f6d3e7aa1
Suraj Kumar <kumarsuraj345678@gmail.com>
2024-03-24 00:05:31 +0530

    fixed api key error

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Results from "@/components/Results";
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.API_KEY_IMDB_NEXT;
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "fetchTrending";
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY_IMDB_NEXT}&language=en-US&page=1`,
+    }?api_key=${API_KEY}&language=en-US&page=1`,
     { next: { revalidate: 10000 } }
   );
   const data = await res.json();
