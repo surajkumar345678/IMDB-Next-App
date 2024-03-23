b5ca902a862d9b19e05fc8f1df9fd4c721434898
Suraj Kumar <kumarsuraj345678@gmail.com>
2024-03-23 22:57:09 +0530

    corrected search keyword

--- src/app/search/[searchTerm]/page.jsx
+++ src/app/search/[searchTerm]/page.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Results from '@/components/Results';
 
 export default async function SearchPage({ params }) {
-  const seachTerm = params.searchTerm;
+  const searchTerm = params.searchTerm;
   const res = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${seachTerm}&language=en-US&page=1&include_adult=false`
+    `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${searchTerm}&language=en-US&page=1&include_adult=false`
   );
   const data = await res.json();
   const results = data.results;
